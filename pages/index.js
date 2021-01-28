4080285caf915eed2ba84974f178b020b6cc75de
jusso-dev <jusso15@gmail.com>
2021-01-28 22:56:28 +1100

    Rewrite of backend to use SDK and add filters for PROTECTED controls

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,16 @@ export default function Home() {
   const [error, setError] = useState("");
 
   const searchQuery = async (data) => {
-    let res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/ism?queryLimit=${data.numberOfResults}&searchQuery=${data.searchTerm}`)
+
+    let url = ''
+    if(data.PROTECTED) {
+      url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/ism?searchFilters=(PROTECTED = Yes AND OFFICAL = No)`
+    }
+    else {
+      url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/ism?queryLimit=${data.numberOfResults}&searchQuery=${data.searchTerm}`
+    }
+
+    let res = await fetch(url)
     if (res.ok) {
       let json = await res.json()
       setResults(json)
@@ -48,18 +57,23 @@ export default function Home() {
 
         <form onSubmit={handleSubmit(searchQuery)}>
           
-          {errors.searchTerm && <p className={styles.span}>This field is required</p>}
-
           <label>
               Enter search term here.. ie. 0041 or CISO
-              <input placeholder={"Enter search term..."} style={{ borderRadius: '25px', width: '100%', padding: '12px 20px', margin: '8px 0px', border: '2px solid black' }} name="searchTerm" ref={register({ required: true })} />
+              <input placeholder={"Enter search term..."} style={{ borderRadius: '25px', width: '100%', padding: '12px 20px', margin: '8px 0px', border: '2px solid black' }} name="searchTerm" ref={register({ required: false })} />
           </label>
           <label>
               Limit results
               <br />
-              <input defaultValue={10} style={{ borderRadius: '25px', width: '20%', padding: '12px 20px', margin: '8px 0px', border: '2px solid black' }} type="number" name="numberOfResults" ref={register({ required: true })} min="1" max="800"></input>
+              <input defaultValue={10} style={{ borderRadius: '25px', width: '20%', padding: '12px 20px', margin: '8px 0px', border: '2px solid black' }} type="number" name="numberOfResults" ref={register({ required: false })} min="1" max="800"></input>
           </label>
-        
+          <br />
+          <label>
+              Show me only PROTECTED controls
+              <input type={'checkbox'} style={{height:'15px', width:'15px'}} name="PROTECTED" ref={register({ required: false })} />
+          </label>
+          
+          <br />
+          <br />
           <button style={{ fontWeight:'bold', borderRadius: '25px', backgroundColor: '#4ecca3', border: 'none', color: '#eeeeee', padding: '16px 32px', width: '100%' }} type="submit">Submit</button>
         </form>
 
