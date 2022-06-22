dad1b9d2a03589d44adf68fd9e8c84986615b569
Justin Middler [MSFT] <Jusso15@gmail.com>
2022-06-22 19:39:47 +1000

    force webpack 5 for netlify

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
+  webpack5: true,
   // Target must be serverless
   target: "serverless",
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback.fs = false;
     }
 
     return config
