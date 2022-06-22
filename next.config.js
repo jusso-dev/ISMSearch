294e7f690b850ed99a1b076c15aca90abc35d31f
Justin Middler [MSFT] <Jusso15@gmail.com>
2022-06-22 18:34:23 +1000

    Fixed message when no results

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-    // Target must be serverless
-    target: "serverless",
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty'
-        }
-      }
-  
-      return config
+  target:"serverless",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
     }
-};
+    return config;
+  },
+}
