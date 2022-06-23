1e3c605ab26ca998c803e572fbce326dfe36ac0f
Justin Middler [MSFT] <Jusso15@gmail.com>
2022-06-23 13:26:36 +1000

    fix webpack issue locally and remote

--- next.config.js
+++ next.config.js
@@ -1,14 +1,30 @@
-module.exports = {
-  // Target must be serverless
-  target: "serverless",
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+// This is need because webpack 5 does not work remotely, only locally
 
-    return config
+let local = process.env.LOCAL ? true : false
+
+if (local) {
+  module.exports = {
+    target:"serverless",
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
   }
-};
+} else {
+  module.exports = {
+    // Target must be serverless
+    target: "serverless",
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+  
+      return config
+    }
+  };
+}
